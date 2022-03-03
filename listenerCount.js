4f9995d41ee1384b8035c4925e31e03608ffac7c
fia-nso <popekleib@gmail.com>
2022-03-03 18:35:11 +0100

    mon projet

---
+++ listenerCount.js
@@ -0,0 +1,8 @@
+var events = require('events');
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+eventEmitter.emit('connection');
+// count the number of listener to this event
+eventListeners = require('events').EventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + " Listner(s) listening to connection event");
